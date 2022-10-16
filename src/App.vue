98bf633f9da938b2e953c4a1666598a0c1cc6d48
1nobugnobug <798526098@qq.com>
2022-10-16 20:57:49 +0800

    feat(global): 新增修改主题功能

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,30 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a
-      href="https://vitejs.dev"
-      target="_blank"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      />
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      /></a>
+    <el-switch 
+      v-model="theme" 
+      active-value="light"
+      inactive-value="dark"
+      @change="changeThemeType"
+    ></el-switch>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script lang='ts' setup>
+import { ref, onMounted } from 'vue'
+
+const theme = ref('light')
+const changeThemeType = (type:string) => {
+    if (theme.value !== type) {
+        theme.value = type
+    }
+    localStorage.setItem('easy-viaualize-theme', type)
+    document.documentElement.className = theme.value
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+onMounted(() => {
+    changeThemeType(localStorage.getItem('easy-viaualize-theme') || theme.value)
+})
+
+</script>
+<style scoped lang='scss'>
+
 </style>
